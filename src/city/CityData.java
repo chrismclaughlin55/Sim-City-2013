ad7fef01e26d4fc753b222ebdbd3cce0e02bd513
Brian Kim <byk.briankim@gmail.com>
2013-11-17 16:47:29 -0800

    CityData skeleton and description added

---
+++ src/city/CityData.java
@@ -0,0 +1,9 @@
+package city;
+
+public class CityData {
+	/*
+	CityData will hold all information regarding bus routes,
+	restaurants available and their type, and whatever else
+	makes it easier or should be global data known, such as time.
+	*/
+}
