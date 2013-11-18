6d262592c5fb1d7cc8d05561edc11614442d42a5
cemclaug <chrismclaughlin55@gmail.com>
2013-11-17 17:44:12 -0800

    skeleton for bank roles

---
+++ src/bank/CustomerRole.java
@@ -0,0 +1,19 @@
+package bank;
+
+import city.PersonAgent;
+import city.Role;
+
+public class CustomerRole extends Role {
+
+	public CustomerRole(PersonAgent person) {
+		super(person);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/bank/TellerRole.java
@@ -0,0 +1,17 @@
+package bank;
+import city.*;
+
+public class TellerRole extends Role{
+
+	public TellerRole(PersonAgent person) {
+		super(person);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
