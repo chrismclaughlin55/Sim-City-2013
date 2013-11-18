ce3977979ab6078050f3e06bf7347896a8a74428
cemclaug <chrismclaughlin55@gmail.com>
2013-11-17 18:11:58 -0800

    skeleton bank roles

---
+++ src/bank/BankManagerRole.java
@@ -0,0 +1,19 @@
+package bank;
+
+import city.PersonAgent;
+import city.Role;
+
+public class BankManagerRole extends Role {
+
+	public BankManagerRole(PersonAgent person) {
+		super(person);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/bank/TellerRole.java
@@ -0,0 +1,19 @@
+package bank;
+
+import city.PersonAgent;
+import city.Role;
+
+public class TellerRole extends Role {
+
+	public TellerRole(PersonAgent person) {
+		super(person);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
