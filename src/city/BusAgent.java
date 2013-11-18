bb2ff7c03fc859df3ecfe4493f8d32d48f074900
Brian Kim <byk.briankim@gmail.com>
2013-11-17 19:08:45 -0800

    Added some meat to the bus things, speaking of meat... dinner time

--- src/city/BusAgent.java
+++ src/city/BusAgent.java
@@ -2,7 +2,17 @@ package city;
 
 public class BusAgent {
 
+	public enum BusState { moving };
 	public BusAgent() {
-		
+		BusState bs;
+		BusStopAgent curr;
+		BusStopAgent next;
+		//SHOULD ALSO HAVE A DEFAULT STARTING POSITION
 	}
+	
+	//MESSAGES
+	
+	//SCHEDULER
+	
+	//ACTIONS
 }
--- src/city/BusStopAgent.java
+++ src/city/BusStopAgent.java
@@ -1,8 +1,16 @@
 package city;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class BusStopAgent {
 
+	BusStopAgent next;
+	List<PersonAgent> waitingPeople;
 	public BusStopAgent() {
-		
+		waitingPeople = new ArrayList<PersonAgent>();
 	}
+	
+	
 }
