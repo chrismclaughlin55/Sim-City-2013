cbb382544a1b8d7bac627ad8ce7890fbca81e0f1
Brian Kim <byk.briankim@gmail.com>
2013-11-18 19:08:38 -0800

    Basic config parser action calls MainGui addPerson method when it parses a line in config.txt

--- src/config/ConfigParser.java
+++ src/config/ConfigParser.java
@@ -1,4 +1,5 @@
 package config;
+import mainGUI.MainGui;
 import java.io.*;
 import java.util.*;
 public class ConfigParser{
@@ -8,13 +9,18 @@ public class ConfigParser{
 	 */
 	//File config = new File("config.txt");
 	Scanner scan;
-	public ConfigParser() throws FileNotFoundException {
+	MainGui main;
+	public ConfigParser(MainGui m) throws FileNotFoundException {
 		//ConfigParser cp = new ConfigParser();
 		scan = new Scanner( new File ("config.txt"));
+		main = m;
 	}
 	
 	public void ParseAndCreatePeople() {
-		if(scan.hasNext()) {
+		if(scan.hasNextLine()) {
+			String name = scan.next();
+			String title = scan.next();
+			main.addPerson(name,title);
 			//MAKE A New Person OBJECT
 			//WORK WITH MAINGUI TO CREATE OBJECT WITH THESE PROPERTIES THAT ARE PARSED IN
 			
