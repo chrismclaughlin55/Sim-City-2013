d3ef85d5972e2015dba2842727c1a5c47725b65b
Brian Kim <byk.briankim@gmail.com>
2013-11-18 19:36:12 -0800

    Added messages and basic Gui components for BusAgent need to make fixes to BusStop and also added stuff to CityData

--- src/city/CityData.java
+++ src/city/CityData.java
@@ -1,6 +1,31 @@
 package city;
-
+import java.util.*;
 public class CityData {
+	
+	class Coordinate {
+		int x;
+		int y;
+		public Coordinate(int x,int y) {
+			this.x=x;
+			this.y=y;
+		}
+		public int getX() {
+			return x;
+		}
+		public int getY() {
+			return y;
+		}
+	}
+	
+	public static Map<BusStopAgent,Coordinate> busStopPositions; //given a busstopagent, will return pixel position in city.
+	public static ArrayList<BusStopAgent> busStops; //has busstops in order of the route the bus will take.
+	public static ArrayList<BusAgent> buses;
+	public static ArrayList<Building> buildings;
+	//ALSO needs a 2-d array of the entire place
+	
+	public CityData() {
+		
+	}
 	/*
 	CityData will hold all information regarding bus routes,
 	restaurants available and their type, and whatever else
---
+++ src/city/gui/BusGui.java
@@ -0,0 +1,34 @@
+package city.gui;
+
+import java.awt.Graphics2D;
+
+import city.BusAgent;
+
+public class BusGui implements Gui {
+	private BusAgent agent = null;
+
+	@Override
+	public void updatePosition() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public boolean isPresent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public void DoGoToNextStop(int x, int y) {
+		
+		
+	}
+}
