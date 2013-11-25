16641f0193d5fe42b891029fa30051bcc83cacd0
Chris McLaughlin <chrismclaughlin55@gmail.com>
2013-11-24 23:43:13 -0800

    Partially implemented TellerRole, CustRole, ManagerRole

--- src/bank/TellerRole.java
+++ src/bank/TellerRole.java
@@ -5,18 +5,29 @@ import bank.interfaces.Teller;
 import bank.utilities.CustInfo;
 import city.PersonAgent;
 import city.Role;
-
+/*
+ *  Added events, states, customer info. TODO actions, scheduler
+ */
 public class TellerRole extends Role implements Teller{
-
+	String name;
+	BankManagerRole bm;
+	CustInfo currentCustInfo;
+	enum State {available, waitingForInfo, waitingForResponse, doneWithCustomer, customerDecidingLoan}
+	enum Event {none, recievedHello, recievedInfo, recievedDeposit,updatedBank,loanReq, iTakeIt}
+	State state;
+	Event event;
 	public TellerRole(PersonAgent person) {
 		super(person);
+		this.name = person.getName();
+		state = State.available;
+		event = Event.none;
 		// TODO Auto-generated constructor stub
 	}
 	
 	//MESSAGES
 	@Override
 	public void msgHello(String name, BankCustomer c) {
-		// TODO Auto-generated method stub
+		currentCustInfo = new CustInfo(name, c.returnPerson() );
 		
 	}
 
@@ -44,6 +55,12 @@ public class TellerRole extends Role implements Teller{
 	public boolean pickAndExecuteAnAction() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public void iTakeIt(double loanAmount) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
