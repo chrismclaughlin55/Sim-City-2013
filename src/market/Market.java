8842df20a198417684e46fac128dc9477f581f93
Kartik Chillakanti <chillaka@usc.edu>
2013-11-26 13:27:55 -0800

    merge commit

--- src/market/Market.java
+++ src/market/Market.java
@@ -1,5 +1,9 @@
 package market;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.swing.JFrame;
 
 import mainGUI.MainGui;
@@ -10,6 +14,7 @@ import market.gui.MarketGui;
 import city.Building;
 import city.CityData;
 import city.PersonAgent;
+import city.Role;
 
 public class Market extends Building {
 
@@ -23,7 +28,7 @@ public class Market extends Building {
 	public Market(int xPos, int yPos, int width, int height, MainGui mainGui) {
 		super(xPos, yPos, width, height, mainGui);
 		this.mainGui = mainGui;
-		
+
 	}
 
 	public Market(int i, int j, int width, int height, String string, BuildingType apartment, MainGui mainGui, CityData cd) {
@@ -31,106 +36,116 @@ public class Market extends Building {
 		//public Market(int xPos, int yPos, int width, int height, String name, BuildingType type, MainGui mainGui, CityData cd) {
 		cityData = cd;
 		this.mainGui = mainGui;
-		
+
 		MarketData chickenData = new MarketData("Chicken", 10, 5.99);
 		MarketData saladData = new MarketData("Salad", 10, 3.99);
 		MarketData steakData = new MarketData("Steak", 10, 11.99);
 		MarketData pizzaData = new MarketData("Pizza", 10, 7.99);
-		
-		
+
+
 		marketGui = new MarketGui(this);
-        marketGui.setTitle("Market");
-        marketGui.setVisible(false);
-        marketGui.setResizable(false);
-        marketGui.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        
+		marketGui.setTitle("Market");
+		marketGui.setVisible(false);
+		marketGui.setResizable(false);
+		marketGui.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
 		inventory = new Inventory(chickenData, saladData, steakData, pizzaData, marketGui);
 		marketGui.updateMarketPanel();
 	}
 
-	public void EnterBuilding(PersonAgent person, String roleRequest) {
-		if (isOpen()) {
-			if (existingRoles.get(person) != null) {
-				person.msgAssignRole(existingRoles.get(person));
-				if (roleRequest.equals("MarketCustomer")) {
-					CustomerGui customerGui = new CustomerGui((MarketCustomerRole)existingRoles.get(person));
-					((MarketCustomerRole) existingRoles.get(person)).setGui(customerGui);
-					marketGui.animationPanel.addGui(customerGui);
-					//currentManager.msgNeedToOrder((MarketCustomerRole) existingRoles.get(person));
-				}
-				if (roleRequest.equals("MarketEmployee")) {
-					currentManager.msgReportingForWork((MarketEmployeeRole) existingRoles.get(person));
-					EmployeeGui employeeGui = new EmployeeGui((MarketEmployeeRole) existingRoles.get(person));
-					((MarketEmployeeRole) existingRoles.get(person)).setGui(employeeGui);
-					marketGui.animationPanel.addGui(employeeGui);
-				}
-				if (roleRequest.equals("MarketManager")) {
-					ManagerGui managerGui = new ManagerGui((MarketManagerRole) existingRoles.get(person));
-					((MarketManagerRole) existingRoles.get(person)).setGui(managerGui);
-					marketGui.animationPanel.addGui(managerGui);
-				}
 
+	protected Map<PersonAgent, MarketManagerRole> existingManagerRoles = Collections.synchronizedMap(new HashMap<PersonAgent, MarketManagerRole>());
+	protected Map<PersonAgent, MarketCustomerRole> existingCustomerRoles = Collections.synchronizedMap(new HashMap<PersonAgent, MarketCustomerRole>());
+	protected Map<PersonAgent, MarketEmployeeRole> existingEmployeeRoles = Collections.synchronizedMap(new HashMap<PersonAgent, MarketEmployeeRole>());
+
+
+
+
+	public void EnterBuilding(PersonAgent person, String roleRequest) {
+
+		if (person.equals(manager)) {
+
+			if (existingManagerRoles.get(person) != null) {
+				currentManager = existingManagerRoles.get(person);
+				ManagerGui managerGui = new ManagerGui(existingManagerRoles.get(person));
+				currentManager.setGui(managerGui);
+				marketGui.animationPanel.addGui(managerGui);
+				person.msgAssignRole(currentManager);
 			}
-			else if (roleRequest.equals("MarketCustomer")) {
-				System.out.println("creating a market customer");
-				MarketCustomerRole custRole = new MarketCustomerRole(person, currentManager, person.thingsToOrder);
-				CustomerGui customerGui = new CustomerGui(custRole);
-				custRole.setGui(customerGui);
-				existingRoles.put(person, custRole);
-				marketGui.animationPanel.addGui(customerGui);
-				person.msgAssignRole(custRole);
+			else {
+				currentManager = new MarketManagerRole(person, inventory, this);
+				existingManagerRoles.put(person,currentManager);
+				ManagerGui managerGui = new ManagerGui(currentManager);
+				currentManager.setGui(managerGui);
+				marketGui.animationPanel.addGui(managerGui);
+				person.msgAssignRole(currentManager);
 			}
 		}
-		else {
-			if (roleRequest.equals("MarketEmployee")) {
+
+		else if (roleRequest.equals("MarketEmployee")) {
+			if (existingEmployeeRoles.get(person) != null) {
+				EmployeeGui employeeGui = new EmployeeGui(existingEmployeeRoles.get(person));
+				existingEmployeeRoles.get(person).setGui(employeeGui);
+				marketGui.animationPanel.addGui(employeeGui);
+				person.msgAssignRole(existingEmployeeRoles.get(person));
+				currentManager.msgReportingForWork(existingEmployeeRoles.get(person));
+			}
+			else {
 				MarketEmployeeRole employeeRole = new MarketEmployeeRole(person, currentManager, inventory);
-				person.msgAssignRole(employeeRole);
-				existingRoles.put(person, employeeRole);
-				currentManager.msgReportingForWork(employeeRole);
+				existingEmployeeRoles.put(person,employeeRole);
 				EmployeeGui employeeGui = new EmployeeGui(employeeRole);
 				employeeRole.setGui(employeeGui);
 				marketGui.animationPanel.addGui(employeeGui);
-			}
-			else if (person.equals(manager)) {
-				if (existingRoles.get(person) != null) {
-					currentManager = (MarketManagerRole) existingRoles.get(person);
-					ManagerGui managerGui = new ManagerGui(currentManager);
-					((MarketManagerRole) existingRoles.get(person)).setGui(managerGui);
-					mainGui.marketGui.animationPanel.addGui(managerGui);
-					person.msgAssignRole(existingRoles.get(person));
+				person.msgAssignRole(employeeRole);
+				currentManager.msgReportingForWork(employeeRole);
 
-				}
-				else {
-					currentManager = new MarketManagerRole(person, inventory, this);
-					person.msgAssignRole(currentManager);
-					existingRoles.put(person, currentManager);
-					ManagerGui managerGui = new ManagerGui(currentManager);
-					currentManager.setGui(managerGui);
-					marketGui.animationPanel.addGui(managerGui);
-				}
 			}
-			//else p.msgAccessDenied()
 		}
 
+		else if (roleRequest.equals("MarketCustomer")) {
+
+			if (isOpen()) {
+
+				if (existingCustomerRoles.get(person) != null) {
+					CustomerGui customerGui = new CustomerGui(existingCustomerRoles.get(person));
+					existingCustomerRoles.get(person).setGui(customerGui);
+					marketGui.animationPanel.addGui(customerGui);
+					person.msgAssignRole(existingCustomerRoles.get(person));
+				}
+				else {
+					MarketCustomerRole customerRole = new MarketCustomerRole(person, currentManager, person.thingsToOrder);
+					existingCustomerRoles.put(person,customerRole);
+					CustomerGui customerGui = new CustomerGui(customerRole);
+					customerRole.setGui(customerGui);
+					marketGui.animationPanel.addGui(customerGui);
+					person.msgAssignRole(customerRole);
+				}
+			}
+		}
 	}
+
 
 	public void test(){
 
 		if (testIteration < 2) {
-
 			System.out.println ("testing market");
-			PersonAgent p1 = new PersonAgent("Manager");
-			p1.startThread();
-			manager = p1;
-			EnterBuilding(p1, "MarketManager");
 
-			PersonAgent p2 = new PersonAgent("Employee");
-			p2.startThread();
-			EnterBuilding(p2, "MarketEmployee");
+			if (testIteration < 1) {
+				PersonAgent p1 = new PersonAgent("Manager");
+				p1.startThread();
+				manager = p1;
+				EnterBuilding(p1, "MarketManager");
 
-			PersonAgent p3 = new PersonAgent("Customer");
-			p3.startThread();
-			EnterBuilding(p3, "MarketCustomer");
+				PersonAgent p2 = new PersonAgent("Employee");
+				p2.startThread();
+				EnterBuilding(p2, "MarketEmployee");
+			}
+
+			if (isOpen()) {
+				PersonAgent p3 = new PersonAgent("Customer");
+				p3.startThread();
+				EnterBuilding(p3, "MarketCustomer");
+			}
 
 			testIteration++;
 		}
